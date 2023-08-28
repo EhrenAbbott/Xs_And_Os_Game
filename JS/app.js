4bc31a2b706fe0a61a2f231526b504a4886254f5
Ehren Abbott <era12358@gmail.com>
2023-08-28 15:32:56 -0500

    Added turnIndicator method

--- JS/app.js
+++ JS/app.js
@@ -149,19 +149,18 @@ const App = {
 function init() { 
     const view = new View() 
      
-    view.bindGameResetEvent(event => { 
+    view.bindGameResetEvent((event) => { 
         console.log('Reset event')
         console.log(event)
     }) 
      
-    view.bindNewRoundEvent(event => { 
+    view.bindNewRoundEvent((event) => { 
         console.log('New round event')
         console.log(event)
     })
 
-    view.bindPlayerMoveEvent(event => { 
-        console.log('Player move event')
-        console.log(event)
+    view.bindPlayerMoveEvent((event) => { 
+        view.setTurnIndicator(2)
     })
 } 
 
